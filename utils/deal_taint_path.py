886ac4e921da9004f7b2f713f9932458ac5469c1
cjc <49013706+cjc-github@users.noreply.github.com>
2023-02-12 21:13:06 +0800

    Update deal_taint_path.py

--- utils/deal_taint_path.py
+++ utils/deal_taint_path.py
@@ -7,11 +7,6 @@ from utils.exception import HaveException, ToolsException
 from utils.utils import changeAmdtoFlow, changeFlowtoAmd, get_type_index
 
 
-# 处理引擎生成的污点,并保存到Taint.txt中
-# 打开taint.txt文件，对sink语句进行分析，如果存在自定义类型，则删除该污点
-# 八种包装类和字符串除外
-
-
 def nocomplex(line):
     str1 = line.split("(")[1].split(">")[0]. \
         replace("Ljava/lang/String;", "T"). \
@@ -27,18 +22,13 @@ def nocomplex(line):
         return False
     return True
 
-
-# sink污染参数为0
 def othersink(param):
     # if int(param[-4:-2]) == 0:
     if int(param.split(" ")[-1][:-2]) == 0:
         return False
     return True
 
-
-# 对这个文件进行处理，如果sink除了复杂类型外还有其他的普通apk，则返回true
 def iscomplex(args_appdata):
-    # 分析AppData.txt文件
     list = []
     with open(args_appdata, "r") as f:
         lines = f.readlines()
@@ -50,8 +40,6 @@ def iscomplex(args_appdata):
                 flag = 0
     f.close()
 
-    # 将非复杂类型的污点路径保存到taint.txt文件中
-    # 复杂类型也考虑吧
     print("    [-] generate the taint.txt")
     taint_txt = args_appdata.replace("AppData.txt", "Taint.txt")
     with open(taint_txt, "w") as f:
@@ -76,20 +64,16 @@ def deal_Amandroid_taint(data_path, apk_name):
         if not os.path.exists(args_data_result):
             raise ToolsException(" [+] " + apk_name + " \n Amandroid not generate the result folder.")
         else:
-            # JN-SAF运行后的AppData.txt的目录
             args_appdata = os.path.join(args_data_result, "AppData.txt")
             if not os.path.exists(args_appdata):
                 raise ToolsException(" [+] " + apk_name + " \n Amandroid not generate the AppData.txt.")
             else:
-                # 进行txt分析
                 with open(args_appdata, "r") as f:
                     lines = f.readlines()
                     last_line = lines[-1]
                     if "Discovered taint paths are listed below:" in str(last_line):
-                        # 没有污点
                         raise ToolsException(" [+] " + apk_name + " \n Amandroid not find the taintpath.")
                     elif "Taint analysis result:" in str(last_line):
-                        # 分析失败
                         raise ToolsException(" [+]" + apk_name + " \n Amandroidnot find the source and sink.")
                 f.close()
                 # have outcome
@@ -97,11 +81,6 @@ def deal_Amandroid_taint(data_path, apk_name):
     else:
         raise ToolsException(" [+] " + apk_name + " \n Amandroid not analysis.")
 
-
-# 处理jucify,
-# 1、只有cg_before和cg_after都存在，才分析
-# 2、before有这个source，after中有source，sink
-# 3、我们将source加入到taint.txt中
 def deal_jucify_taint():
     return
 
@@ -110,7 +89,6 @@ def getcon_method(mth):
     return "<" + mth.split("<")[1].split(">")[0] + ">"
 
 
-# 获取参数对应的下标
 def getcon_para(paras, taint_paras):
     if ", " not in paras:
         if paras[1:-1] == taint_paras:
@@ -125,9 +103,7 @@ def getcon_para(paras, taint_paras):
     return 0
 
 
-# 处理flowdroid,这里就是将xml文件生成对应的script文件
 def deal_flowdroid_taint(flowdroid_folder, app_name):
-    # 先分析是否存在
     leak_file = os.path.join(flowdroid_folder, app_name + "_leak.xml")
     if not os.path.exists(leak_file):
         print(" [*] Flowdroid analysis failed.")
@@ -145,7 +121,6 @@ def deal_flowdroid_taint(flowdroid_folder, app_name):
                 print(" [*] Results is empty")
                 raise HaveException("FlowDroid can't find the taint path")
             else:
-                # 求出Taint.txt文件中的信息
                 taintlist = []
                 file = os.path.join(flowdroid_folder.rsplit("/", 1)[0], "Data", app_name, "result", "Taint.txt")
                 if not os.path.exists(file):
@@ -173,12 +148,10 @@ def deal_flowdroid_taint(flowdroid_folder, app_name):
                 utils.create_floder(os.path.join(scripts, app_name))
                 num = len(os.listdir(os.path.join(scripts, app_name)))
 
-                # 再分析Amandroid是否已经分析出这个leak
                 for result in Results:
                     Sources = result.childNodes[1].childNodes
                     Sink = result.childNodes[0]
                     for Source in Sources:
-                        # 对每一条leak信息进行处理
                         con_source = Source.getAttribute("Statement")
                         con_sink = Sink.getAttribute("Statement")
                         con_taint_par = Sink.childNodes[0].getAttribute("Value")
@@ -189,7 +162,6 @@ def deal_flowdroid_taint(flowdroid_folder, app_name):
                                 flag = 1
                                 print(" [*][*][*] The path is common the Amandroid.\n\n\n")
                                 # raise HaveException("The path is common of Amandroid and FlowDroid.")
-                        # 添加到script中,先判断script目录啥的，然后添加信息，以script_f1开头
                         if flag == 0:
                             x = changeFlowtoAmd(getcon_method(con_source))
                             y = changeFlowtoAmd(getcon_method(con_sink))
@@ -208,7 +180,6 @@ def deal_flowdroid_taint(flowdroid_folder, app_name):
                                 else:
                                     f.write("method:" + y + " nostatic\n")
                                 f.write("{\n")
-                                # 参数
                                 for i in para_list:
                                     if i[0] == z:
                                         f.write(
@@ -251,7 +222,6 @@ def deal_jnfuzz_test(outpath, decompile_path, app_name):
                 else:
                     f.write("method:" + y + " nostatic\n")
                 f.write("{\n")
-                # 参数
                 for k, v in enumerate(create_types(method)[0]):
                     f.write("p" + str(k) + ":{is_tainted:false, value:default, type:" + v + "}\n")
                 f.write("}\n")
